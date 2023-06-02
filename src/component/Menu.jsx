0268cf33f9f0fca3841d25d43c31367c3061a627
Bopanna99 <bopannachettimada@gmail.com>
2023-06-02 14:19:37 +0530

    Imported Toastify

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { logoutUser } from "../data/user";
+import { toast } from 'react-toastify'
 
 function Menu(props){
 
